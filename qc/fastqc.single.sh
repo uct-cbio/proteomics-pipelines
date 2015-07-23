43c3acabe0c1df1fb7d11d375f0c983e74a2867a
Gerrit Botha <gerrit.botha@uct.ac.za>
2015-07-23 16:25:19 +0200

    Now echoing job commands to *.cmds log.

--- qc/fastqc.single.sh
+++ qc/fastqc.single.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 . $config
-$fastqc_base/fastqc $fastq -f fastq -o $out_dir -t $fastqc_threads
+
+echo "" > $cmds_log
+
+cmd="$fastqc_base/fastqc $fastq -f fastq -o $out_dir -t $fastqc_threads"
+
+echo $cmd >> $cmds_log
+`eval $cmd`
+
