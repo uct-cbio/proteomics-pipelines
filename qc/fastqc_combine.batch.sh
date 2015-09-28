bc88fdba96fe814445a29922079e3dc20cd632b6
Gerrit Botha <gerrit.botha@uct.ac.za>
2015-09-28 15:12:55 +0200

    Now only keeping PBS job id echo.

--- qc/fastqc_combine.batch.sh
+++ qc/fastqc_combine.batch.sh
@@ -10,10 +10,6 @@ else
   . $config
 fi
 
-echo $in_dir
-echo $out_dir
-echo $log_dir
-
 log_dir=$log_dir"/fastqc_combine."`date +"%y%m%d%H%M%S"`
 
 if [ ! -d $log_dir ];
@@ -24,10 +20,10 @@ fi
 count=1
 
 cmds_log=$log_dir/fastqc_combine.$count.cmds
-qsub="qsub -N fastqc_combine.$count -M $pbs_status_mail_to -m $pbs_status_mail_events -o $log_dir/fastqc_combine.$count.o -e $log_dir/fastqc_combine.$count.e -d $out_dir -q $pbs_queue -S /bin/bash -l nodes=1:$pbs_series:ppn=$fastqc_combine_threads -l walltime=$fastqc_combine_walltime  -v config=$config,cmds_log=$cmds_log ./fastqc_combine.single.sh"
+qsub="qsub -N fastqc_combine.$count -M $pbs_status_mail_to -m $pbs_status_mail_events -o $log_dir/fastqc_combine.$count.o -e $log_dir/fastqc_combine.$count.e -d $out_dir -q $pbs_queue -S /bin/bash -l nodes=1:$pbs_series:ppn=$fastqc_combine_threads -l walltime=$fastqc_combine_walltime  -v config=$config,cmds_log=$cmds_log $scripts_dir/fastqc_combine.single.sh"
 
 echo $qsub > $log_dir/fastqc_combine.$count.qsub
-cat fastqc_combine.single.sh > $log_dir/fastqc_combine.$count.sh
+cat $scripts_dir/fastqc_combine.single.sh > $log_dir/fastqc_combine.$count.sh
 
 if [ $fastqc_combine_DEBUG -eq 1 ]
 then
--- qc/fastqc_combine.single.sh
+++ qc/fastqc_combine.single.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 . $config
 
-echo "" > $cmds_log
+echo -n "" > $cmds_log
 
-$fastqc_combine_base/fastqc_combine.pl -v --out $out_dir --skip --files "$out_dir/*_fastqc"
+cmd="$fastqc_combine_base/fastqc_combine.pl -v --out $out_dir --skip --files \"$out_dir/*_fastqc\""
 
 echo $cmd >> $cmds_log
-`eval $cmd
+eval $cmd
