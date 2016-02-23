09d23939e8d84e6fb0df62892784761bd6921421
Gerrit Botha <gerrit.botha@uct.ac.za>
2016-02-23 23:36:26 +0200

    In the latest FastQC it seems that the option --extract needs to be added so that the output folders are created. The fastqc_combine.pl scripts needs those folders. It is however strange that this option is needed because the FastQC -help output says that this should be enabled by default in interactive mode.

--- qc/fastqc.single.sh
+++ qc/fastqc.single.sh
@@ -3,7 +3,7 @@
 
 echo -n "" > $cmds_log
 
-cmd="$fastqc_base/fastqc $fastq -f fastq -o $out_dir -t $fastqc_threads"
+cmd="$fastqc_base/fastqc --extract $fastq -f fastq -o $out_dir -t $fastqc_threads"
 
 echo $cmd >> $cmds_log
 eval $cmd
