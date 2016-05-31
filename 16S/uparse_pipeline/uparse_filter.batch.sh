16f33285d76cb171448a6552559700c9a0de514e
Katie Viljoen <katie.viljoen@uct.ac.za>
2016-05-31 14:39:12 +0000

    uparse_filter.batch.sh edited online with Bitbucket (changed 'uparse_filter.KLv2.batch.sh' to 'uparse_filter.batch.sh')

--- 16S/uparse_pipeline/uparse_filter.batch.sh
+++ 16S/uparse_pipeline/uparse_filter.batch.sh
@@ -40,15 +40,13 @@ do
   #KL changed to:
   filtered_1_fasta=$out_dir/$sid.merged.filtered_1.fasta #this is for the first filtering step (max_e filtering) before primer stripping
   filtered_2_fasta=$out_dir/$sid.merged.filtered_2.fasta #this is for the second filtering step: primer stripping
-  filtered_3_fasta=$out_dir/$sid.merged.filtered_3.fasta #this is for the third filtering step: length trunctation
-#  if [ ! -f $filtered_3_fasta ]
-#  then  
+  filtered_3_fasta=$out_dir/$sid.merged.filtered_3.fasta #this is for the third filtering step: length trunctation 
   cmds_log=$log_dir/uparse_filter.$sid.$count.cmds
 
     #KL changed to:
-  qsub="qsub -N uparse_filter.$sid.$count -o $log_dir/uparse_filter.$sid.$count.o -e $log_dir/uparse_filter.$sid.$count.e -d $out_dir -q $pbs_queue -S /bin/bash -l nodes=1:$pbs_series:ppn=$uparse_filter_threads -l walltime=$uparse_filter_walltime -v config=$config,merged_fastq=$merged_fastq,filtered_1_fasta=$filtered_1_fasta,fastq_maxee=$uparse_filter_fastq_maxee,filtered_2_fasta=$filtered_2_fasta,mapping_file=$mapping_file,primer_strip_log=$log_dir/SP_log.$sid.$count.txt,filtered_3_fasta=$filtered_3_fasta,max_len=$max_len,min_len=$min_len,target_len=$target_len,cmds_log=$cmds_log $scripts_dir/uparse_filter.KLv2.single.sh"
+  qsub="qsub -N uparse_filter.$sid.$count -o $log_dir/uparse_filter.$sid.$count.o -e $log_dir/uparse_filter.$sid.$count.e -d $out_dir -q $pbs_queue -S /bin/bash -l nodes=1:$pbs_series:ppn=$uparse_filter_threads -l walltime=$uparse_filter_walltime -v config=$config,merged_fastq=$merged_fastq,filtered_1_fasta=$filtered_1_fasta,fastq_maxee=$uparse_filter_fastq_maxee,filtered_2_fasta=$filtered_2_fasta,mapping_file=$mapping_file,primer_strip_log=$log_dir/SP_log.$sid.$count.txt,filtered_3_fasta=$filtered_3_fasta,max_len=$max_len,min_len=$min_len,target_len=$target_len,cmds_log=$cmds_log $scripts_dir/uparse_filter.single.sh"
   echo $qsub > $log_dir/uparse_filter.$sid.$count.qsub
-  cat $scripts_dir/uparse_filter.KLv2.single.sh > $log_dir/uparse_filter.$sid.$count.sh
+  cat $scripts_dir/uparse_filter.single.sh > $log_dir/uparse_filter.$sid.$count.sh
 
   if [ $uparse_filter_DEBUG -eq 1 ]
    then
@@ -60,9 +58,7 @@ do
    fi
 
    (( count+=1 ))
-  # else
-  #  echo "UPARSE filter ouput for "$sid" exists already. Skipping this sample." 
-  # fi
+
 
 done < $sid_fastq_pair_list
 
