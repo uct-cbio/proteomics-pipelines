1ede30d6ecd934c64f0f7b14b1738133ea0ef495
Gerrit Botha <gerrit.botha@uct.ac.za>
2016-05-31 11:52:21 +0200

    Added Katies' strip primer step. Some additional code are required and stored here (/opt/exp_soft/qiime/packages/other/primer_stripping/strip_primers_hex.py, /opt/exp_soft/qiime/packages/other/primer_stripping/truncate_seq_lens.py). The code also requires that the Python version needs to be changed between steps. We need to sort this out at some point so that this Python version switching is not required.

--- 16S/uparse_pipeline/uparse_filter.batch.sh
+++ 16S/uparse_pipeline/uparse_filter.batch.sh
@@ -2,6 +2,7 @@
 
 set -x
 
+#if the first column is empty it prints the message "Please provide a config"
 if [ "$1" == "" ]
 then
   echo "Please provide a config"
@@ -33,17 +34,25 @@ count=1
 while read sid_fastq_pair;
 do 
   sid=`echo $sid_fastq_pair | awk -F ' ' '{print $1}'` # It is actually not reading it as a TAB. Maybe the read operation replaces the TABs with spaces.
-
-  merged_fastq=$in_dir/$sid.merged.fastq
-  filtered_fasta=$out_dir/$sid.merged.filtered.fasta
-
-  if [ ! -f $filtered_fasta ]
+#KL: the above line gets the sample ID from the sid_fastq_pair file
+  merged_fastq=$in_dir/$sid.merged.fastq #here the sid created in the above line is concatenated with .merged.fastq at the end (a '.' seperates variables from other stuff)
+  #filtered_fasta=$out_dir/$sid.merged.filtered.fasta
+  #KL changed to:
+  filtered_1_fasta=$out_dir/$sid.merged.filtered_1.fasta #this is for the first filtering step (max_e filtering) before primer stripping
+  filtered_2_fasta=$out_dir/$sid.merged.filtered_2.fasta #this is for the second filtering step: primer stripping
+  filtered_3_fasta=$out_dir/$sid.merged.filtered_3.fasta #this is for the third filtering step: length trunctation
+  
+  #if [ ! -f $filtered_fasta ]
+  #KL changed to:
+  if [ ! -f $filtered_3_fasta ]
   then
 
     cmds_log=$log_dir/uparse_filter.$sid.$count.cmds
-    qsub="qsub -N uparse_filter.$sid.$count -M $pbs_status_mail_to -m $pbs_status_mail_events  -o $log_dir/uparse_filter.$sid.$count.o -e $log_dir/uparse_filter.$sid.$count.e -d $out_dir -q $pbs_queue -S /bin/bash -l nodes=1:$pbs_series:ppn=$uparse_filter_threads -l walltime=$uparse_filter_walltime -v config=$config,merged_fastq=$merged_fastq,filtered_fasta=$filtered_fasta,fastq_trunclen=$uparse_filter_fastq_trunclen,fastq_maxee=$uparse_filter_fastq_maxee,cmds_log=$cmds_log $scripts_dir/uparse_filter.single.sh"
+   # qsub="qsub -N uparse_filter.$sid.$count -M $pbs_status_mail_to -m $pbs_status_mail_events  -o $log_dir/uparse_filter.$sid.$count.o -e $log_dir/uparse_filter.$sid.$count.e -d $out_dir -q $pbs_queue -S /bin/bash -l nodes=1:$pbs_series:ppn=$uparse_filter_threads -l walltime=$uparse_filter_walltime -v config=$config,merged_fastq=$merged_fastq,filtered_fasta=$filtered_fasta,fastq_trunclen=$uparse_filter_fastq_trunclen,fastq_maxee=$uparse_filter_fastq_maxee,cmds_log=$cmds_log $scripts_dir/uparse_filter.single.sh"
+    #KL changed to:
+    qsub="qsub -N uparse_filter.$sid.$count -o $log_dir/uparse_filter.$sid.$count.o -e $log_dir/uparse_filter.$sid.$count.e -d $out_dir -q $pbs_queue -S /bin/bash -l nodes=1:$pbs_series:ppn=$uparse_filter_threads -l walltime=$uparse_filter_walltime -v config=$config,merged_fastq=$merged_fastq,filtered_1_fasta=$filtered_1_fasta,fastq_maxee=$uparse_filter_fastq_maxee,filtered_2_fasta=$filtered_2_fasta,mapping_file=$mapping_file,primer_strip_log=$log_dir/SP_log.$sid.$count.txt,filtered_3_fasta=$filtered_3_fasta,fastq_trunclen=$uparse_filter_fastq_trunclen,cmds_log=$cmds_log $scripts_dir/uparse_filter.single_KL_primer_strip.sh"
     echo $qsub > $log_dir/uparse_filter.$sid.$count.qsub
-    cat $scripts_dir/uparse_filter.single.sh > $log_dir/uparse_filter.$sid.$count.sh
+    cat $scripts_dir/uparse_filter.single_KL_primer_strip.sh > $log_dir/uparse_filter.$sid.$count.sh
 
    if [ $uparse_filter_DEBUG -eq 1 ]
     then
--- 16S/uparse_pipeline/uparse_filter.single.sh
+++ 16S/uparse_pipeline/uparse_filter.single.sh
@@ -4,7 +4,27 @@
 echo -n "" > $cmds_log
 
 # All stdout and stderr will go to individual per job *.o and *.e, so will not be piping to a log file.
-cmd="$uparse_base/usearch7 -fastq_filter $merged_fastq -fastq_trunclen $fastq_trunclen -fastq_maxee $fastq_maxee -fastaout $filtered_fasta"
+#Quality filter based on E scores
+if [ ! -f $filtered_1_fasta ]
+then
+  cmd="$uparse_base/usearch7 -fastq_filter $merged_fastq  -fastq_maxee $fastq_maxee -fastaout $filtered_1_fasta"
+  echo $cmd >> $cmds_log
+  eval $cmd
+fi
 
-echo $cmd >> $cmds_log
-eval $cmd
+#Now strip primers
+if [ ! -f $filtered_2_fasta ]
+then
+  module add python/anaconda-python-2.7
+  cmd="python /opt/exp_soft/qiime/packages/other/primer_stripping/strip_primers_hex.py $mapping_file $filtered_1_fasta $filtered_2_fasta $primer_strip_log" 
+  echo $cmd >> $cmds_log
+  eval $cmd
+fi
+
+#Now do absolute length truncation
+if [ ! -f $filtered_3_fasta ]
+then
+  cmd="/opt/exp_soft/qiime/python-2.7.3/bin/python  /opt/exp_soft/qiime/packages/other/primer_stripping/truncate_seq_lens.py $filtered_2_fasta 250 260 250 $filtered_3_fasta"
+  echo $cmd >> $cmds_log
+  eval $cmd
+fi
