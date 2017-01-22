e9e134d070321743b4f872a5bc19ef472e49348b
thys <thys@ame-4.local.lan>
2017-01-22 09:13:25 +0200

    GBROWSE docker config

--- proteogenomics/bin/docker/gbrowse/run.sh
+++ proteogenomics/bin/docker/gbrowse/run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 ./build.sh
-docker run -it --rm --net='host' cbio/gbrowse2:latest
+docker run -it --rm --net='host' --name cbio_gbrowse2 -v $HOME/bio/gbrowse/data:/data cbio/gbrowse2:latest
+#docker run -it --rm --net='host' -v $HOME/bio/gbrowse/data:/data cbio/gbrowse2:latest /bin/bash
