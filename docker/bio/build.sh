cc253f1ae466456091305ffcaa5121137ff73190
ThysPotgieter <thys@cognitivesystems.co.za>
2019-10-11 22:02:46 +0200

    new bio comntain er

--- docker/bio/build.sh
+++ docker/bio/build.sh
@@ -1,1 +1,11 @@
-docker build --build-arg ftp_proxy=$ftp_proxy --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -t cbio/bio:latest .
+#/usr/bin/env bash
+set -e
+
+# Change build context to the root of the directory
+version=v1.0
+name=bio
+docker pull thyscbio/${name}:${version} && echo "version exists" && exit 1 || echo "Gonna build."
+cd ../..
+sudo docker build --build-arg ftp_proxy=$ftp_proxy --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -f docker/${name}/Dockerfile  -t thyscbio/${name}:${version} .
+sudo docker login https://index.docker.io/v1/thyscbio
+sudo docker push  thyscbio/${name}:${version}
