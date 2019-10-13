6e483061ac6c6dbbd0e637f3a60296f9aad006bb
Matthys Potgieter <matthys.potgieter@gmail.com>
2019-10-13 21:54:40 +0200

    optparse fix

--- singularity/mqmetaproteomics/create_image.sh
+++ singularity/mqmetaproteomics/create_image.sh
@@ -1,2 +1,5 @@
-version=v2.1
+version=v2.2
+export SINGULARITY_CACHEDIR=/cbio/users/ptgmat003/singularity # comment this out if unsure 
+export SINGULARITY_TMPDIR=/cbio/users/ptgmat003/singularity/tmp
+
 singularity build mqmetaproteomics_${version}.img docker://thyscbio/mqmetaproteomics:${version}
