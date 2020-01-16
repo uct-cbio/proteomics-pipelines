27172333c1d50f4b5264e5edac3d977f9565f7b6
Matthys Potgieter <matthys.potgieter@gmail.com>
2020-01-16 12:27:34 +0200

    mqmetaproteomics

--- singularity/mqmetaproteomics/create_image.sh
+++ singularity/mqmetaproteomics/create_image.sh
@@ -1,4 +1,4 @@
-version=v2.4
+version=v2.5
 
 export SINGULARITY_CACHEDIR=/cbio/users/ptgmat003/singularity # comment this out if unsure 
 export SINGULARITY_TMPDIR=/cbio/users/ptgmat003/singularity/tmp
