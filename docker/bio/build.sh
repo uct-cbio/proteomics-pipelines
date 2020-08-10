6375fd01a5034962774ab5a8d0f4f9c383c21fdf
ThysPotgieter <thys@cognitivesystems.co.za>
2020-08-10 13:58:42 +0200

    proteomics backup made

--- docker/bio/build.sh
+++ docker/bio/build.sh
@@ -2,11 +2,13 @@
 set -e
 
 # Change build context to the root of the directory
-version=v1.4
-
+version=v1.8
 name=bio
 docker pull thyscbio/${name}:${version} && echo "version exists" && exit 1 || echo "Gonna build."
 cd ../..
 sudo docker build --build-arg ftp_proxy=$ftp_proxy --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -f docker/${name}/Dockerfile  -t thyscbio/${name}:${version} .
 sudo docker login https://index.docker.io/v1/thyscbio
-sudo docker push  thyscbio/${name}:${version}
+sudo docker push  thyscbio/${name}:${version} || exit 1
+
+sudo docker tag thyscbio/${name}:${version} thyscbio/${name}:latest
+sudo docker push  thyscbio/${name}:latest
