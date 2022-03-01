75106b80d8f85af6746902499cf2a5f07cfab5c6
Matthys Potgieter <matthys.potgieter@gmail.com>
2022-03-01 15:43:22 +0200

    thys

--- singularity/mqproteogenomics/create_image.sh
+++ singularity/mqproteogenomics/create_image.sh
@@ -1,2 +1,4 @@
-version=v1.4
+version=v1.5
+export SINGULARITY_CACHEDIR=/cbio/users/ptgmat003/singularity # comment this out if unsure 
+export SINGULARITY_TMPDIR=/cbio/users/ptgmat003/singularity/tmp
 singularity build mqproteogenomics_${version}.img docker://thyscbio/mqproteogenomics:${version}
