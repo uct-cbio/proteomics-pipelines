535e2ee1743f05ea50618361651a4ef7fe2a0022
Matthys Potgieter <matthys.potgieter@gmail.com>
2023-08-12 14:13:45 +0000

    pytest

--- docker/bio/build.sh
+++ docker/bio/build.sh
@@ -2,13 +2,13 @@
 set -e
 
 # Change build context to the root of the directory
-version=v3.0.4
+version=v3.0.5
 name=bio
 docker pull thyscbio/${name}:${version} && echo "version exists" && exit 1 || echo "Gonna build."
 cd ../..
-sudo docker build --build-arg ftp_proxy=$ftp_proxy --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -f docker/${name}/Dockerfile  -t thyscbio/${name}:${version} .
-sudo docker login https://index.docker.io/v1/thyscbio
-sudo docker push  thyscbio/${name}:${version} || exit 1
+docker build --build-arg ftp_proxy=$ftp_proxy --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -f docker/${name}/Dockerfile  -t thyscbio/${name}:${version} .
+docker login https://index.docker.io/v1/thyscbio
+docker push  thyscbio/${name}:${version} || exit 1
 
-sudo docker tag thyscbio/${name}:${version} thyscbio/${name}:latest
-sudo docker push  thyscbio/${name}:latest
+docker tag thyscbio/${name}:${version} thyscbio/${name}:latest
+docker push  thyscbio/${name}:latest
--- docker/mqproteogenomics/dev.sh
+++ docker/mqproteogenomics/dev.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -a
-version=v2.0.4
+version=v3.0.5
 home=/home/$(who -m | awk '{print $1;}')
 config=${home}/.proteomics-pipelines_profile
 source $config
+
 DEV_PYTHONPATH=${PROTEOMICS_PIPELINES_PATH}/lib/
 
 DEV_PATH="$PATH::${PROTEOMICS_PIPELINES_PATH}/bin/python/:${PROTEOMICS_PIPELINES_PATH}/bin/R:${PROTEOMICS_PIPELINES_PATH}/bin/bash/:${PROTEOMICS_PIPELINES_PATH}/bin/perl/"
 
-docker run -it --rm -p 8181:9090 -e PYTHONPATH=${DEV_PYTHONPATH} -e PATH=${DEV_PATH} -v ${CBIO_PROTEOGENOMICS_TESTS_PATH}:${CBIO_PROTEOGENOMICS_TESTS_PATH} -v ${PROTEOMICS_PIPELINES_PATH}:${PROTEOMICS_PIPELINES_PATH} thyscbio/mqproteogenomics:${version}  /bin/bash 
+docker run -it --rm -p 8181:9090 -e PYTHONPATH=${DEV_PYTHONPATH} -e PATH=${DEV_PATH} -v ${CBIO_PROTEOGENOMICS_TESTS_PATH}:${CBIO_PROTEOGENOMICS_TESTS_PATH} -v ${PROTEOMICS_PIPELINES_PATH}:${PROTEOMICS_PIPELINES_PATH} thyscbio/bio:${version}  /bin/bash 
