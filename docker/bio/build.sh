683314ab767327d7837a6700185ccee83ca8bafd
Matthys Potgieter <matthys.potgieter@gmail.com>
2024-12-27 14:32:39 +0000

    thys

--- docker/bio/build.sh
+++ docker/bio/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Change build context to the root of the directory
-version=v3.0.19
+version=v4.0.0
 name=bio
 docker pull thyscbio/${name}:${version} && echo "version exists" && exit 1 || echo "Gonna build."
 cd ../..
