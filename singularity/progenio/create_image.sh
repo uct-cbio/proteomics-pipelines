92c2d85e2645dd5ea570ded37ef09f96912b69a6
Matthys Potgieter <matthys.potgieter@gmail.com>
2024-12-31 22:16:26 +0200

    progenio v1.0.0

--- singularity/progenio/create_image.sh
+++ singularity/progenio/create_image.sh
@@ -1,4 +1,4 @@
 version=v1.0.0
 export SINGULARITY_CACHEDIR=/cbio/users/ptgmat003/singularity # comment this out if unsure 
 export SINGULARITY_TMPDIR=/cbio/users/ptgmat003/singularity/tmp
-singularity build progenix_${version}.img docker://thyscbio/progenix:${version}
+singularity build progenio_${version}.img docker://thyscbio/progenio:${version}
